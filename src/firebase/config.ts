0effc2614eeeffe0b1ed4d345bad346710add286
Araval Csalud <csaludaraval@gmail.com>
2025-11-02 15:10:47 +0000

    dejemos en standby preocupacional.aravalcsalud.cl de momento y ttrabajem

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ import { FirebaseOptions, initializeApp, getApps, getApp } from 'firebase/app';
 // Your web app's Firebase configuration
 export const firebaseConfig: FirebaseOptions = {
   apiKey: 'AIzaSyDSx_gjlw5TJgUDgvnowJDyWtid4eTgrzE',
-  authDomain: 'preocupacional.aravalcsalud.cl',
+  authDomain: 'studio-4288557503-eb161.firebaseapp.com',
   projectId: 'studio-4288557503-eb161',
   storageBucket: 'studio-4288557503-eb161.appspot.com',
   messagingSenderId: '848859591437',
