c84e058172be170957218163d68f010e49d315d4
Araval Csalud <csaludaraval@gmail.com>
2025-11-02 14:47:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4288557503-eb161",
+  "appId": "1:848859591437:web:d30ccd9d0461c3db2bf644",
+  "apiKey": "AIzaSyDSx_gjlw5TJgUDgvnowJDyWtid44TgrzE",
+  "authDomain": "studio-4288557503-eb161.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "848859591437"
+};
