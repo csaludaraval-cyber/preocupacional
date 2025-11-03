aaf46d4345224c0b12faef739e394256135728af
Araval Csalud <csaludaraval@gmail.com>
2025-11-03 03:58:03 +0000

    pero como programador deber generar aquello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/layout/Header';
 import { CrearCotizacion } from '@/components/cotizacion/CrearCotizacion';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import AITimer from '@/components/AITimer';
 
 
 export default function Home() {
@@ -9,7 +10,10 @@ export default function Home() {
         <div className="min-h-screen w-full bg-background">
         <Header />
         <main className="container mx-auto px-4 py-8 md:px-6">
-            <CrearCotizacion />
+            <AITimer />
+            <div className="mt-8">
+              <CrearCotizacion />
+            </div>
         </main>
         </div>
     </FirebaseClientProvider>
